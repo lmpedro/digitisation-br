a97000e0c4a46777dd6c3d96b52edb255d40c18f
Pedro Mendes Loureiro <pedro@Pedros-MacBook-Pro.local>
2014-03-18 09:14:37 -0300

    Versão do dia 17, primeira a incluir método para filtrar os dados e fazer com que sejam referentes ao Brasil. Cabe notar que esta parte está em testes_mlab.
    Coisas a fazer: não sei ao certo.

--- mlab_base_input.py
+++ mlab_base_input.py
@@ -11,20 +11,218 @@ import zipfile
 import tarfile
 import csv
 import subprocess
-from mlab_base import parser, reader, texpand, metalist, createout, downloader, checkindex, updatedelete, roda
 
+#from mlab_base import parser
+#from mlab_base import reader
+#from mlab_base import texpand
+#from mlab_base import metalist
+#from mlab_base import createout
+
+
+def parser(input):
+    inpath=""
+    temppath=input.split("/")
+    for x in range(len(temppath)-1):
+        inpath+=str(temppath[x])+"/"
+    
+    tempname=temppath[-1].split(".")
+    inname=tempname[-2]
+    return inpath,inname
+
+def reader(input):
+    output={}
+    file = open(input,"r")
+    
+    for x in file:
+        x = x.split(":")
+        header=x[0]
+        if header==" * Additional data": break
+        results=""
+        for i in range(1,len(x)):
+            if i>1: results=results+":"
+            results=results+x[i]
+        results = results.replace("\n","")
+        results = results.replace(" ","")
+        output[header]=results
+    
+    web100 = output["Summary data"].split(",")
+    if len(web100)!=53:
+        return 0
+    else:
+        i=3
+        for x in web100:
+            header="web100_%02i" %i
+            output[header]=x
+            i+=1
+        output.pop("Summary data")
+        return output
+
+def texpand(input,extraction=1):
+    ttotal=time.time()
+    tfil=time.time()
+    print "\nExtracting filenames..."
+    targuy=tarfile.open(input,"r")
+    namelist=targuy.getnames()
+    tfil=time.time()-tfil
+    print "Extracted filenames in: ",tfil," seconds."
+    if extraction!=0:
+        text=time.time()
+        print "Extracting files..."
+        inpath,inname=parser(input)
+        for f in targuy:
+            try:
+                targuy.extract(f,inpath)
+            except IOError as e:
+                os.remove(inpath+f.name)
+                targuy.extract(f,inpath)
+        text=time.time()-text
+        print "Extracted files in: ",text," seconds."
+    return namelist
+
+def metalist(input):
+    output=[]
+    for x in input:
+        jon = x.split(".")
+        if jon[-1]=="meta": output.append(x)
+    return output
+
+def createout(output,input):
+    print "\nCreating summary file..."
+    outfile = open(output,"w")
+    listed=[]
+    i=0
+    sortee=sorted(input)
+    for key in sortee:
+        listed.append([])
+        listed[i].append(key)
+        for x in input[key]:
+            listed[i].append(x)
+        i+=1
+        transposed=[]
+        for k in range(len(listed[0])):
+            row=[]
+            for j in range(len(listed)):
+                row.append(listed[j][k])
+            transposed.append(row)
+    for h in range(len(transposed)):
+        for i in range(len(transposed[h])):
+            if i>0: outfile.write("|")
+            outfile.write(transposed[h][i])
+        outfile.write('\n')
+    outfile.close()
+
+def downloader(data,destino,index):
+    tglobal=time.time()
+    print "\nInitiating download process..."
+    
+    gspath = 'gs://m-lab/ndt/' + data + '/'
+    tarlist = subprocess.check_output('gsutil ls ' + gspath, shell=True)
+    tarlist = tarlist.split('\n')
+    print "There are a total of %i files for this date. Working on file %i (index %i)." %(len(tarlist)-1,index+1,index)
+    if index >= len(tarlist)-1 :
+        raise ValueError, "This is an invalid index file. Probably, all the files for this date have been processed. Terminating the programme."
+    
+    chamada = 'gsutil cp ' + tarlist[index] + " " + destino
+    subprocess.call(chamada, shell=True)
+    
+    fpath = tarlist[index]
+    fname = fpath.split("/")
+    fname = str(fname[-1])
+    local = destino + fname
+    
+    tglobal = time.time()-tglobal
+    print "Total download time: " + str(tglobal) + " seconds."
+    
+    return local
+
+def checkindex(data,destino,index):
+    update=0
+    if index == "l":
+        update = 1
+        try:
+            indexator = open(destino+data+"/indexator.txt","r")
+            index = long(indexator.read())
+            indexator.close()
+        except IOError:
+            print "The indexator does not exist. Starting, therefore, from file index 0."
+            index = 0
+    return index, update
+
+def updatedelete(update,destino,data,index,filelist,tarpath):
+    if update == 1:
+        indexator = open(destino+data+"/indexator.txt","w")
+        index += 1
+        indexator.write(str(index))
+        indexator.close()
+    
+    for x in filelist:
+        x=destino+x
+        os.remove(x)
+    os.remove(tarpath)
+
+
+
+def roda(data,destino,index):
+    troda=time.time()
+    
+    index,update = checkindex(data,destino,index)
+    
+    print "\nInitiating the program for " + data + ", file index " + str(index) + "."
+    tarpath = downloader(data,destino,index)
+    filelist = texpand(tarpath)
+    mfilelist = metalist(filelist)
+    
+    if mfilelist == [] :
+        updatedelete(update,destino,data,index,filelist,tarpath)
+        return
+    
+    tester = 0
+    intermediate = 0
+    while intermediate == 0:
+        intermediate=reader(destino+mfilelist[tester])
+        tester+=1
+    
+    final={}
+    for x in intermediate:
+        final[x]=[]
+    
+    for x in mfilelist:
+        input=destino+x
+        intermediate=reader(input)
+        if intermediate==0:
+            continue
+        for y in intermediate:
+            final[y].append(intermediate[y])
+    
+    creationpath=destino+"Results/"+data+"/summary_%03i.txt" %index
+    if os.path.exists(destino+"Results/"+data+"/") == False: os.makedirs(destino+"Results/"+data+"/")
+    createout(creationpath,final)
+    
+    updatedelete(update,destino,data,index,filelist,tarpath)
+    
+    troda=time.time()-troda
+    print "\nThe program took " + str(troda) + " seconds."
+
+print "Enter date (YYYY/MM/DD):"
 date = raw_input()
-print date
-'''
-    while 666 > 1:
-    try: roda('2014/03/02','/Users/pedro/CTI/ID/MLab/NDT/',"l")
+
+datelist=date.split('/')
+year=datelist[0]
+month=datelist[1]
+day=datelist[2]
+
+assert int(year)>=2009 and int(year)<=2014 and len(year)==4
+assert int(month)>=1 and int(month)<=12 and len(month)==2
+assert int(day)>=1 and int(day)<=31 and len(day)==2
+
+while 666 > 1:
+    try: roda(date,'/Users/pedro/CTI/ID/MLab/NDT/',"l")
     except IOError:
-    print "\nThere seems to have occurred an IOError. Probably, something went awry with the download. Let us give it another try"
-    continue
+        print "\nThere seems to have occurred an IOError. Probably, something went awry with the download. Let us give it another try"
+        continue
     except ValueError:
-    print "\nThis is an invalid index file. Probably, all the files for this date have been processed. Terminating the programme. This is probably the end, mate, start working on another date!!"
-    break
+        print "\nThis is an invalid index file. Probably, all the files for this date have been processed. Terminating the programme. This is probably the end, mate, start working on another date!!"
+        break
     except KeyError:
-    print "A KeyError, you don't say? This should nay have happened. It matters not: let us merely run this shit again."
-    continue
-'''
+        print "A KeyError, you don't say? This should nay have happened. It matters not: let us merely run this shit again."
+        continue
--- testes_mlab.py
+++ testes_mlab.py
@@ -11,85 +11,159 @@ import zipfile
 import tarfile
 import urllib2
 import subprocess
+import maxminddb as geo
+import socket
+import io
 
-url="gs://m-lab/ndt/2014/01/01/20140101T000000Z-mlab4-nuq01-ndt-0000.tgz"
+def localizer(geo,IP):
+    try:
+        location = geo.get(IP)
+        x = IP
+    except ValueError:
+        try:
+            x = socket.gethostbyname(IP)
+            location = geo.get(x)
+        except socket.gaierror:
+            location = ''
+            x = ''
+    return location, x
 
-'''
-#subprocess.call('gsutil cp gs://m-lab/ndt/2014/02/01/20140201T000000Z-mlab4-nuq01-ndt-0001.tgz "/Volumes/External Data/"', shell=True)
-noia='gs://m-lab/ndt/2014/02/01/'
-testao=subprocess.check_output('gsutil ls '+noia, shell=True)
+def falsedic(entry,list):
+    i = 0
+    for x in list:
+        if x == entry: return i
+        i+=1
+    raise ValueError
 
-for x in testao:
-    print x
+def setcount(list):
+    lset = set(list)
+    countdic = {}
+    for x in lset:
+        countdic[x] = 0
+    for x in list:
+        countdic[x] += 1
+    return countdic
 
-teste=testao.split("\n")
+def reducegeo(geo):
+    reduced=[]
+    try:
+        reduced.append(geo["city"]["geoname_id"])
+    except KeyError:
+        reduced.append('')
+    try:
+        reduced.append(geo["city"]["names"]["pt-BR"])
+    except KeyError:
+        reduced.append('')
+    try:
+        reduced.append(geo["location"]["latitude"])
+    except KeyError:
+        reduced.append('')
+    try:
+        reduced.append(geo["location"]["longitude"])
+    except KeyError:
+        reduced.append('')
+    return reduced
 
-print teste
+def brazilianiser(tests, geoobject,br):
+    counter=[0,0]
+    clist=[]
+    btests=[]
+    geoindex = falsedic("client IP address",tests[0])
+    for j in range(len(tests)):
+        place, red_ip = localizer(geoobject,tests[j][geoindex])
+        if place == '' or place == None:
+            counter[1]+=1
+        else:
+            try:
+                clist.append(place["country"]["iso_code"])
+                if place["country"]["iso_code"] == "BR":
+                    br+=1
+                    btests.append(tests[j])
+                    red_ip = [red_ip]
+                    btests.append(red_ip)
+                    rplace=reducegeo(place)
+                    for x in rplace:
+                        btests[-1].append(x)
+                    print btests[-1]
+                counter[0] += 1
+            except TypeError:
+                print "There was a TypeError, you see."
+                counter[1] += 1
+                print place
+            except KeyError:
+                print "There was a KeyError, you see."
+                counter[1] += 1
+                print place
+    print "\nSo far, br is "+str(br)
+    print "So for our clist, ..."
+    clistcounted = setcount(clist)
+    print clistcounted
+    print "We have had %i %i hits and misses." % (counter[0], counter[1])
+    return btests, br
 
-'''
-def downloader(data,destino,primeiro,ultimo):
-    ultimo+=1
-    primeiro-=1
-    gspath = 'gs://m-lab/ndt/' + data + '/'
-    tarlist = subprocess.check_output('gsutil ls ' + gspath, shell=True)
-    tarlist = tarlist.split('\n')
-    for index in range(primeiro,ultimo):
-        chamada = 'gsutil cp ' + tarlist[index] + " " + destino
-        subprocess.call(chamada, shell=True)
-    locallist=[]
-    for index in range(primeiro,ultimo):
-        fpath = tarlist[index]
-        fname = fpath.split("/")
-        fname = str(fname[-1])
-        locallist.append(destino + fname)
-    return locallist
+def createout(output,input,header):
+    print "\nCreating summary file..."
+    outfile = io.open(output,"w",encoding='utf8')
+    #total=header+input
+    header.append("true_IP")
+    header.append("city_geoname_id")
+    header.append("city_name")
+    header.append("latitude")
+    header.append("longitude")
+    total = []
+    total.append(header)
+    for x in input:
+        total.append(x)
+    i=0
+    print total
+    for h in range(len(total)):
+        for i in range(len(total[h])):
+            if i>0: outfile.write(u"|")
+            print total[h][i]
+            outfile.write(unicode(total[h][i]))
+        outfile.write(u'\n')
+    outfile.close()
 
-#jeack = downloader('2014/02/01','/Users/pedro/Downloads/',1,5)
+database="/Users/pedro/CTI/ID/GeoLite2 dloaded march 12/GeoLite2-City.mmdb"
 
-#print jeack
+gi = geo.Reader(database)
 
+basepath="/Users/pedro/CTI/ID/MLab/NDT/Results/"
+data="2014/03/01"
+rdirectory = basepath+data+"/"
 
-'''
-    basepath="/Users/pedro/CTI/ID/MLab/NDT/"
-    tar=basepath+"20140201T000000Z-mlab1-ams01-ndt-0000.tgz"
-    
-    filelist=texpand(tar,1)
-    mfilelist=metalist(filelist)
-    print mfilelist
-    print len(mfilelist)
-    
-    input=basepath+mfilelist[0]
-    int=reader(input)
-    final={}
-    for x in int:
-    final[x]=[]
-    
-    for x in mfilelist:
-    input=basepath+x
-    int=reader(input)
-    for y in int:
-    final[y].append(int[y])
-    
-    #os.rmdir(basepath+"2012/01/05/")
-    #os.rmdir(basepath+"2012/01/")
-    #os.rmdir(basepath+"2012/")
-    
-    print "Final length is " + str(len(final))
-    i=1
-    for x in final:
-    print "Then we go on to " + str(i) + " " + str(len(final[x]))
-    print "By the way, this is " + x
-    i+=1
-    
-    print final["web100_33"]
-    print len(final["web100_33"])
-    createout(tar+".csv",final)
-    
-    for x in filelist:
-    x=basepath+x
-    os.remove(x)
-    '''
+rlist = os.listdir(rdirectory)
 
-try: ack=open("/Users/pedro/aaa.txt","r")
-except IOError:
-    print "The indexator does not exist. Starting, therefore, from file index 0."
+br=0
+btests=[]
+joe=0
+for summary in rlist:
+    if joe>10:break
+    if joe<9:
+        joe+=1
+        continue
+    filetime = time.time()
+    file = open(rdirectory+summary,"r")
+
+    tests = []
+    for x in file:
+        tests.append(x.split("|"))
+
+    file.close()
+
+    for h in range(len(tests)):
+        for i in range(len(tests[h])):
+            tests[h][i]=tests[h][i].replace('\n','')
+
+    inttest, br = brazilianiser(tests,gi,br)
+    if inttest!=[]:
+        for x in range(len(inttest)):
+            btests.append(inttest[x])
+    filetime = time.time()-filetime
+    print "%i tests in %i seconds. Average number of tests per second: %.1f." % (len(tests),filetime,(len(tests)/filetime))
+    print "We finished file %s. There are %i files." % (summary,len(rlist))
+    joe+=1
+print btests
+finalcsv="/Users/pedro/CTI/ID/MLab/NDT/Results/test.txt"
+
+createout(finalcsv,btests,tests[0])
